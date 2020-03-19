a3ee70acd0a7750009d2812ebe26093ade6d4604
matzor <matzor.formo@gmail.com>
2020-03-19 15:35:03 +0100

    Updated files from Blackboard

--- example.m
+++ example.m
@@ -3,12 +3,19 @@ data = load('data/HICO.mat');
 HICO_original = data.HICO_original;
 HICO_noisy = data.HICO_noisy;
 hico_wl = data.hico_wl;
-seawater_Rs = data.seawater_Rs;
+
+land_mask         = double(imread('data/land_mask.png'));
+land_mask         = land_mask(:,:,1);
+deep_water_Rrs    = load('data/deep_water_Rrs.txt');
+shallow_water_Rrs = load('data/shallow_water_Rrs.txt');
+valid_bands_Rrs   = load('data/valid_bands_Rrs.txt');
 
 size(HICO_original)
 size(HICO_noisy)
 size(hico_wl)
-size(seawater_Rs)
+size(deep_water_Rrs)
+size(shallow_water_Rrs)
+size(valid_bands_Rrs)
 
 % To convert a hyperspectral image cube I to matrix form X:
 I = HICO_original;
@@ -18,6 +25,10 @@ X = X';
 
 % To convert a matrix X back into a hyperspectral image cube:
 I = reshape(X', [H,W,L]);
+
+% To set all land pixels to zero:
+% (See https://se.mathworks.com/matlabcentral/answers/38547-masking-out-image-area-using-binary-mask)
+I = bsxfun(@times, I, cast(land_mask, 'like', I));
 
 % Plot a single spectral band
 imagesc(I(:,:,30));
--- example.py
+++ example.py
@@ -6,12 +6,19 @@ M             = loadmat('data/HICO.mat')
 HICO_original = M['HICO_original'] # Hyperspectral image cube
 HICO_noisy    = M['HICO_noisy']    # HICO_original with added noise
 hico_wl       = M['hico_wl']       # Physical wavelength corresponding to band i
-seawater_Rs   = M['seawater_Rs']
+
+land_mask         = plt.imread('data/land_mask.png')[:,:,0] == 0
+deep_water_Rrs    = np.loadtxt('data/deep_water_Rrs.txt')
+shallow_water_Rrs = np.loadtxt('data/shallow_water_Rrs.txt')
+valid_bands_Rrs   = np.loadtxt('data/valid_bands_Rrs.txt')
 
 print(HICO_original.shape)
 print(HICO_noisy.shape)
 print(hico_wl.shape)
-print(seawater_Rs.shape)
+print(land_mask.shape)
+print(deep_water_Rrs.shape)
+print(shallow_water_Rrs.shape)
+print(valid_bands_Rrs.shape)
 
 # To convert a hyperspectral image cube I to matrix form X:
 I = HICO_original
@@ -21,6 +28,9 @@ X = X.T
 
 # To convert a matrix X back into a hyperspectral image cube:
 I = np.reshape(X.T, [H,W,L])
+
+# To set all land pixels to zero:
+I[land_mask] = 0
 
 # Plot a single spectral band
 plt.imshow(I[:,:,30])
