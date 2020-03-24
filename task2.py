c968d7cdf1e3eac13c098db84ea8d7cfabd3e281
matzor <matzor.formo@gmail.com>
2020-03-24 18:46:24 +0100

    Think task 2c is done

--- task2.py
+++ task2.py
@@ -9,15 +9,21 @@ HICO_original = M['HICO_original'] # Hyperspectral image cube
 HICO_noisy    = M['HICO_noisy']    # HICO_original with added noise
 hico_wl       = M['hico_wl']       # Physical wavelength corresponding to band i
 seawater_Rs   = M['seawater_Rs']
+land_mask     = plt.imread('data/land_mask.png')[:,:,0] == 0
 
 deep_water_Rrs = np.loadtxt('data/deep_water_Rrs.txt')
 shallow_water_Rrs = np.loadtxt('data/shallow_water_Rrs.txt')
+
+Rrs = np.empty((deep_water_Rrs.shape[0], 2))
+Rrs[:,0] = deep_water_Rrs
+Rrs[:,1] = shallow_water_Rrs
 
 print("HICO_original.shape", HICO_original.shape)
 print("HICO_noisy.shape", HICO_noisy.shape)
 print("hico_wl.shape", hico_wl.shape)
 print("seawater_Rs.shape", seawater_Rs.shape)
 print("Rrs.shape", deep_water_Rrs.shape)
+
 
 I = HICO_original
 
@@ -30,7 +36,7 @@ def get_index_of_wavelength(wavelength):
     from math import isclose
     for i in range(hico_wl.shape[0]):
         if isclose(hico_wl[i], wavelength, abs_tol=5.7/2):
-            print("L ", i, hico_wl[i])
+            #print("L ", i, hico_wl[i])
             return i
 
 def kmeans_cluster():
@@ -51,7 +57,7 @@ def kmeans_cluster():
         plt.xlabel("Wavelength [nm]")
         plt.savefig("fig/kmean/kmean_" + str(i) + ".png")
 
-def nasa_obpg(Img):
+def nasa_obpg():
     """ 
     Runs NASA OBPG algorithm with default parameters
     """
@@ -69,6 +75,7 @@ def nasa_obpg(Img):
     ydim = HICO_original.shape[1]
 
     img = np.empty((xdim, ydim))
+    I[land_mask] = 0
     
     for x in range(xdim):
         for y in range(ydim):
@@ -76,89 +83,112 @@ def nasa_obpg(Img):
             for i in range(1, len(a)):
                 img[x, y] += a[i] * np.log10(np.max(I[x, y, i_blue]) / I[x, y, i_green])
     #img = 10 ** img
+    plt.figure()
     plt.imshow(img)
-    plt.savefig("fig/2b_nasa.png")
+    plt.savefig("fig/2b_nasa_cor.png")
     plt.show()
 
-def OLD_calculate_atmospheric_scattering_coefficients(Img, point, Rrs):
-    """ 
-    Calculates the a and b coefficients for estimating 
-    reflectance from the surface through atmosphere
-    Inputs:
-        Img: HICO array
-        point: reference point, ie. deep water or shallow water,
-        (x, y) coordinates
-        Rrs: Calibrated remote sensing reflectance
-        Wavelength: 
-    Returns:
-        a, b
-    """
-    ones = np.ones(Rrs.shape[0])
-    A = np.empty((Rrs.shape[0], 2))
-    A[:,0] = Rrs
-    A[:,1] = ones
-    B = Img[point[0], point[1], :]
-    B = B.reshape((100, 1))
-    x, _, _, _ = np.linalg.lstsq(A, B)
-    print(x)
-    return x[0, 0], x[1, 0]
 
-def calculate_atmospheric_scattering_coefficients(Img, point, Rrs, i_lambda_start, i_lambda_stop):
+def calculate_atmospheric_scattering_coefficients(Img, points, Rrs, i_lambda_start, i_lambda_stop):
     """ 
     Calculates the a and b coefficients for estimating 
     reflectance from the surface through atmosphere
     Inputs:
         Img: HICO array
-        point: reference point, ie. deep water or shallow water,
+        point: reference points, ie. deep water or shallow water,
         (x, y) coordinates
         Rrs: Calibrated remote sensing reflectance
         i_lambda_start: index of first wavelength
         i_lambda_stop: index of last wavelength
     Returns:
         a, b
+        array containing all a_i, b_i for all lambda_i
+        a_i, b_i is zero for any lambda_i outside (i_lambda_start, i_lambda_stop)
     """
-    # 
-    # TODO: Shouldn't we get an a and b for each wavelength?
-    # Not just one for the whole span?
-    # 
 
     length = i_lambda_stop - i_lambda_start
-    ones = np.ones(length)
-    A = np.empty((length, 2))
-    A[:, 0] = Rrs[i_lambda_start:i_lambda_stop]
-    A[:, 1] = ones
-    B = Img[point[0], point[1], i_lambda_start:i_lambda_stop]
-
-    #print("A: \n", A.shape)
-    #print("B: \n", B.shape)
-
-    X, _, _, _ = np.linalg.lstsq(A, B)
-
-    
     # Get one pair (a, b) for each wavelength:
-    #X = np.empty((length, 2))
-
-    """
+    X = np.zeros((Rrs.shape[0], 2))
     for i in range(length):
-        a = A[i, :]#.reshape((2,1))
-        b = B[i]#.reshape((1, 1))
-        print("A: \n", a)
-        print("B: \n", b)
-        X[i], _, _, _ = np.linalg.solve(a, b) 
-    """
-    print(X)
-    return X[0], X[1]
+        a = np.array([  
+                        [Rrs[i_lambda_start+i, 0],  1],
+                        [Rrs[i_lambda_start+i, 1],  1]
+                    ])
+        b = np.array([  
+                        [Img[points[0, 0], points[0, 1], i_lambda_start + i]],
+                        [Img[points[1, 0], points[1, 1], i_lambda_start + i]]
+                    ])
+        #print("A: \n", a.shape)
+        #print("B: \n", b.shape)
+        x = np.linalg.solve(a, b)
+        #x, _, _, _ = np.linalg.lstsq(a, b)
+        #print("x: \n", x.shape)
+        X[i_lambda_start + i, 0] = x[0, 0]
+        X[i_lambda_start + i, 1] = x[1, 0]
+
+    #print(X)
+    return X[:, 0], X[:, 1]     #a, b
 
 
 
 ## Estimate the reflectance from the surface of the ocean
 
-points = [[20, 20], [100, 70]]      # Deep water, Shallow water
+def run_task_c():
+    points = np.array([[20, 20], [100, 70]])      # Deep water, Shallow water
+    R = 34
+    G = 25
+    B = 8
 
-i_start = get_index_of_wavelength(438)
-i_stop = get_index_of_wavelength(730)
+    i_start = get_index_of_wavelength(438)
+    i_stop = get_index_of_wavelength(730)
 
-a_deep, b_deep = calculate_atmospheric_scattering_coefficients(I, 
-    points[0], deep_water_Rrs, i_start, i_stop)
-a_shallow, b_shallow = calculate_atmospheric_scattering_coefficients(I, 
-    points[1], shallow_water_Rrs, i_start, i_stop)
+    a, b = calculate_atmospheric_scattering_coefficients(I, 
+        points, Rrs, i_start, i_stop)
+
+    plt.figure(1)
+    plt.subplot(121)
+    plt.title("Original")
+    spy.imshow(I, (34, 25, 8), fignum=1)
+
+
+    RGB_d_org = [I[20, 20, R], I[20, 20, G], I[20, 20, B]]
+    RGB_s_org = [I[100, 70, R], I[100, 70, G], I[100, 70, B]]
+
+    R_mean = np.mean(I[:,:,R])
+    G_mean = np.mean(I[:,:,G])
+    B_mean = np.mean(I[:,:,B])
+
+    print("Deep water: ", RGB_d_org, "Shallow water: ", RGB_s_org)
+    
+
+    for i in range(i_start, i_stop):
+        I[:,:,i] = (I[:,:,i] - b[i]) / a[i]
+
+    plt.subplot(122)
+    plt.title("Corrected")
+    spy.imshow(I, (34, 25, 8), fignum=1)
+    plt.savefig("fig/pseudo_rgb_corrected.png")
+    plt.show()
+    
+
+    RGB_d_cor = [I[20, 20, R], I[20, 20, G], I[20, 20, B]]
+    RGB_s_cor = [I[100, 70, R], I[100, 70, G], I[100, 70, B]]
+
+    R_mean_cor = np.mean(I[:,:,R])
+    G_mean_cor = np.mean(I[:,:,G])
+    B_mean_cor = np.mean(I[:,:,B])
+
+    print("Deep water: ", RGB_d_cor, "Shallow water: ", RGB_s_cor)
+
+    print("Color ratio:")
+    for i in range(len(RGB_d_cor)):
+        print(i)
+        print(RGB_d_cor[i] / RGB_d_org[i])
+        print(RGB_s_cor[i] / RGB_s_org[i])
+    
+    print("-"*80)
+    print(R_mean_cor/R_mean, G_mean_cor/G_mean, B_mean_cor/B_mean)
+
+run_task_c()
+
+nasa_obpg()
