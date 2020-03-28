Colors in task 2d is wrong (compared to actual). Made land mask for task 2e, but colors are all wrong after masking

example.py
@@ -1,6 +1,7 @@
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 import numpy as np
+import spectral as spy
 
 M             = loadmat('data/HICO.mat')
 HICO_original = M['HICO_original'] # Hyperspectral image cube
@@ -34,8 +35,8 @@ I[land_mask] = 0
 
 # Plot a single spectral band
 plt.imshow(I[:,:,30])
+#spy.imshow(I, (34, 25, 8))
 plt.show()
-
 # Note that quite a few libraries assume a matrix layout where
 # each row is a spectral vector, rather than each column as in
 # equation 2 of the assignment text. Read the documentation of

task2.py
@@ -24,7 +24,7 @@ print("hico_wl.shape", hico_wl.shape)
 print("seawater_Rs.shape", seawater_Rs.shape)
 print("Rrs.shape", deep_water_Rrs.shape)
 
-
+rgb = (34, 25, 8)
 I = HICO_original
 
 def get_index_of_wavelength(wavelength):
@@ -59,7 +59,8 @@ def kmeans_cluster(I, filename):
 
 def nasa_obpg(I, filename):
     """ 
-    Runs NASA OBPG algorithm with default parameters
+    Runs NASA OBPG algorithm with default parameters,
+    saves result to file
     """
     # Parameters for the NASA OBPG algorithm 
     I_temp = np.empty_like(I)
@@ -78,15 +79,19 @@ def nasa_obpg(I, filename):
     ydim = HICO_original.shape[1]
 
     img = np.empty((xdim, ydim))
-    I_temp[land_mask] = 0
+    #I_temp[land_mask] = 0
     
     for x in range(xdim):
         for y in range(ydim):
             img[x, y] = a[0]
             for i in range(1, len(a)):
                 img[x, y] += a[i] * np.log10(np.max(I_temp[x, y, i_blue]) / I_temp[x, y, i_green])
-    #img = 10 ** img
+    img = 10 ** img
+    #p_min = -2
+    #p_max = 1
     plt.figure()
+    plt.title(filename)
+    #plt.imshow(img, vmax=p_max, vmin=p_min)
     plt.imshow(img)
     plt.savefig("fig/" + filename + ".png")
     
@@ -200,14 +205,95 @@ def atmospheric_correction(I):
 
     return img
 
+def make_land_mask(I):
+    """
+    Creates a land mask, masking out land, showing only water
+    Checks for peaks in pixel intensity after specific wavelength
+    Assuming water has decreasing intensity for increasing wavelength.
+    Also saves land mask as png. 
+    Input:
+        I: Image cube (hyperspectral, HICO)
+    Returns:
+        img: land mask numpy array
+    """
+    # Tweakable parameters: 
+    max_peak = 0.55
+    start_index = get_index_of_wavelength(700)
+    stop_index = get_index_of_wavelength(730)
+    k = 10
+    m, c = spy.kmeans(I, k, 100)
+
+    # Plotting the kmeans result for comparison
+    plt.figure(figsize=(14, 7))
+    plt.subplot(121)
+    plt.title("Pixel colored by cluster, k = " + str(k))
+    plt.imshow(m)
+    plt.subplot(122)
+    plt.title("K-cluster means")
+    legs = range(c.shape[0])
+    for j in range(c.shape[0]):
+        pylab.plot(hico_wl[:,0], c[j])
+    plt.legend(legs)
+    plt.xlabel("Wavelength [nm]")
+    plt.savefig("fig/landmask_kmean.png")
+
+    # Check mean values of k means after start index
+    # Also want i=8 for some reason
+    img = np.zeros_like(m)
+    for i in range(c.shape[0]):
+        print(np.mean(c[i, start_index:stop_index]))
+        if np.mean(c[i, start_index:stop_index]) < max_peak or i==8:
+            print("Found an acceptable class!")
+            for x in range(m.shape[0]):
+                for y in range(m.shape[1]):
+                    if m[x,y] == i:
+                        img[x,y] = 1
+                        
+    plt.figure()
+    plt.imshow(img)
+    plt.savefig("fig/landmask.png")
+    np.save("land_mask", img)
+    return img
+
+def plot_masked_image(I):
+    mask = np.load("land_mask.npy")
+
+    fignr = 5
+    plt.figure(fignr)
+    plt.subplot(121)
+    plt.title("Original RGB")
+    spy.imshow(I, rgb, fignum=fignr)
+
+    plt.subplot(122)
+    plt.title("RGB with land mask")
+
+    #I[land_mask] = 0
+
+    for i in range(hico_wl.shape[0]):
+        I[:,:,i] = I[:,:,i] * mask
+        
+    ### WHY DOES IT CHANGE COLORS???
+
+    spy.imshow(I, rgb, fignum=fignr)
+    #spy.imshow(I, rgb)
+
+    plt.savefig("fig/rgb_masked.png")    
+
+    nasa_obpg(I, "masked_obpg.png")
 
 
-kmeans_cluster(I, "kmean_")
+## doing the stuff 
 
-nasa_obpg(I, "2b_nasa")
+#kmeans_cluster(I, "kmean_")
+
+#nasa_obpg(I, "NASA OBPG, original")
 
 I_cor = atmospheric_correction(I)
 
-nasa_obpg(I_cor, "2d_nasa_corrected")
+#nasa_obpg(I_cor, "NASA OBPG, corrected")
 
-kmeans_cluster(I_cor, "kmean_corrected")
+#kmeans_cluster(I_cor, "kmean_corrected")
+
+#mask = make_land_mask(I_cor)
+
+plot_masked_image(I_cor)